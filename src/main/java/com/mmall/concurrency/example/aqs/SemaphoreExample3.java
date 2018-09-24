a0bfdbefdeb4624b4fc90cf333ac213689cd55ba
madison <zhuangsen@live.com>
2018-09-24 21:40:17 +0800

    ReentrantLock

--- src/main/java/com/mmall/concurrency/example/aqs/SemaphoreExample3.java
+++ src/main/java/com/mmall/concurrency/example/aqs/SemaphoreExample3.java
@@ -2,44 +2,39 @@ package com.mmall.concurrency.example.aqs;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
 
 @Slf4j
-public class CountDownLatchExample2 {
+public class SemaphoreExample3 {
 
-    private final static int threadCount = 200;
+    private final static int threadCount = 20;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        final Semaphore semaphore = new Semaphore(3);
 
         for (int i = 0; i < threadCount; i++) {
             final int threadNum = i;
-//            Thread.sleep(1);
             executorService.execute(()->{
                 try {
-                    test(threadNum);
+                    if(semaphore.tryAcquire()){
+                        test(threadNum);
+                        semaphore.release(3);// 释放一个许可
+                    }
                 } catch (Exception e){
                     log.error("exception", e);
-                } finally {
-                    countDownLatch.countDown();
                 }
             });
         }
-
-        countDownLatch.await(10, TimeUnit.MILLISECONDS);
-        log.info("finish");
         executorService.shutdown();
     }
 
     private static void test(int threadNum) throws Exception{
-        Thread.sleep(100);
         log.info("{}", threadNum);
-//        Thread.sleep(100);
+        Thread.sleep(1000);
     }
 
 }
